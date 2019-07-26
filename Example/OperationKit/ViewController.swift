f45c6a483a502b169ebf60c69c14714cd5df823b
Victor Shabanov <shabanov.dev.git@gmail.com>
2019-07-26 17:22:08 +0300

    Initial commit

---
+++ Example/OperationKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  OperationKit
+//
+//  Created by viveron on 07/26/2019.
+//  Copyright (c) 2019 viveron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
